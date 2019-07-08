7a91e71994f74355ae3a09b00ff055cb861639e4
Anastasiya Krivokora <anastasiya.krivokora@CORP.TASKDATA.RU>
2019-07-08 14:55:15 +0300

    commit

---
+++ 8.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import math
+
+link = "http://suninjuly.github.io/math.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+
+x_element = browser.find_element_by_id('input_value')
+x = x_element.text
+
+
+def calc(x):
+  return str(math.log(ln(abs(12*sin(x)))))
+
+
+y = calc(x)
+
+
+input3 = browser.find_element_by_id('answer')
+input3.send_keys(y) 
+
+option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+option1.click()
+
+option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+option2.click()
