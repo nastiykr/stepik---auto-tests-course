f3e988b5d3154e43b7742bb360a224f1c4400920
Anastasiia <nastiykr@gmail.com>
2019-07-23 23:22:13 +0300

    update file

--- 32.py
+++ 32.py
@@ -3,9 +3,6 @@ from selenium import webdriver
 import time
 import math
 
-answer = math.log(int(time.time()))
-
-
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
@@ -14,8 +11,19 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('intput', ["236895", "236896","236897", "236898", "236899", "236903", "236904", "236905"])
+
+
+
+@pytest.mark.parametrize('input', ["895","896", "897", "898","899", "903", "904", "905"])
 def test_guest_should_see_login_link(browser, input):
-    link = "https://stepik.org/lesson/{}/step/1/".format(input)
-    browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
+	link = "https://stepik.org/lesson/236{}/step/1/".format(input)
+	browser.implicitly_wait(10)
+	browser.get(link)
+	input = browser.find_element_by_css_selector("textarea.textarea")
+	
+	answer = math.log(int(time.time()))
+	input.send_keys(str(answer))
+	
+	button = browser.find_element_by_css_selector(".submit-submission")
+	button.click()
+	assert browser.find_element_by_css_selector(".smart-hints__hint").text == "Correct!"
